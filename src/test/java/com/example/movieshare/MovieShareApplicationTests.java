8f6ada0dbfc2847ebb7a38eae462b339d2cb939b
s22288 <s22288@pjwstk.edu.pl>
2022-07-16 20:30:42 +0200

    Initial commit

---
+++ src/test/java/com/example/movieshare/MovieShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movieshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
